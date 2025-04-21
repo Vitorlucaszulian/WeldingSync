f6a300af8bbe27923abf16ac330b18b26953d408
Vitor Lucas Zulian <vitorlucaszulian@gmail.com>
2025-04-20 21:15:31 -0300

    Alteração

--- app/Main.py
+++ app/Main.py
@@ -1,15 +1,8 @@
 from flask import Flask, render_template, jsonify
-from DataManeger import get_latest_data
+app = Flask(_name_)
 
-app = Flask(__name__)
-
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template("dashboard.html")
+    return "hello from flask"
 
-@app.route('/data')
-def data():
-    return jsonify(get_latest_data())
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+app.run(host = "0.0.0.0")
